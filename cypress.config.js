67b9c17b11be01075b77039f81ba19b2e2063eab
Keremenci <mkmenemencioglu@hotmail.com>
2024-08-21 20:02:19 +0300

    Add cypress tests, remove apiUrl as it is no longer needed

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,5 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    env: {
-      apiUrl: "http://localhost:8080",
-    },
   },
 });
